b5dbc396b3b98bac3870a7c21bbf59e1f468e9be
Eric Garcia <e.garcia@berkeley.edu>
2022-05-30 18:22:03 -0700

    Added extra pointer features including description, doubling FPC. Added a UI element to track FPC and another element to display amount of pointers.

--- src/items.tsx
+++ src/items.tsx
@@ -2,19 +2,36 @@ import {ManipulateFaith} from "./App";
 
 /** Describes a class for an item. Other items inherit from this class. */
 abstract class ItemClass {
-    itemName = ""
-    static baseCost = 0;
-    static multiplier = 1.05;
-    static img = "";
-    static numberOwned = 0;
-    effect = (manipulate: ManipulateFaith) => {};
+    itemName = "";
+    description = "";
+    multiplier = 1.05;
+    baseCost = 0;
+    currCost = () => this.baseCost * this.multiplier ** this.numberOwned;
+    img = "";
+    numberOwned = 0;
+    onClick = (faithTools: ManipulateFaith) => {
+        if (faithTools.faith >= this.currCost()) {
+            faithTools.setFaith(faith => faith - this.currCost())
+            this.numberOwned += 1;
+            this.effect(faithTools);
+        }
+    }
+    effect = (faithTools: ManipulateFaith) => {};
   }
 
-class Pointer {
+class Pointer extends ItemClass{
     itemName = "Pointer";
-    effect(manipulate: ManipulateFaith) {
-        manipulate.setFaith(manipulate.faith + 5);
-        console.log("here")
+    description = "The basic item. Gives you Faith equal to one click"
+    + " and increases FPS by 1. For every 25 bought, Faith per click doubles."
+    baseCost = 10;
+    img = ""
+    effect = (faithTools: ManipulateFaith) => {
+        faithTools.setFaith(faith => faith + faithTools.FPC);
+        faithTools.setFPS(FPS => FPS + 1);
+        if (this.numberOwned % 25 == 0) {
+            faithTools.setFPC((FPC) => FPC * 2);
+            console.log("doubled" + faithTools.FPC)
+        }
     }
 }
 
