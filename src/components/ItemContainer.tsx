3b9e8a4687d2a4fbafeace8bde8dab56042634ea
Eric Garcia <e.garcia@berkeley.edu>
2022-06-03 21:37:00 -0700

    changed item box color and border and added a description on hover

--- src/components/ItemContainer.tsx
+++ src/components/ItemContainer.tsx
@@ -1,6 +1,7 @@
 import ManipulateFaith from '../utils/ManipulateFaith';
-import '../styles/Item-Container.css';
+import '../styles/Items.css';
 import * as items from '../utils/items';
+import { useState } from 'react';
 
 /** Contains all item components. */
 function ItemContainer({faithTools}: {faithTools: ManipulateFaith}) {
@@ -16,16 +17,29 @@ function ItemContainer({faithTools}: {faithTools: ManipulateFaith}) {
   /** Component for an Item, such as a Pointer */
   function Item({item, faithTools}:
                 {item: items.ItemClass, faithTools: ManipulateFaith}) {
+    const [display, setDisplay] = useState(false)
     return (
       <div>
-        <button 
-            className="Item-container"
+        <div
+            className="Item"
             onClick={() => item.onClick(faithTools)}
+            onMouseEnter={() => setDisplay(true)}
+            onMouseLeave={() => setDisplay(false)}
         >
-            {item.itemName + " " + item.currCost()}
-        </button>
+            {item.itemName + " " + Math.floor(item.currCost())}
+        </div>
+        <ItemDescription item={item} display={display}/>
       </div>
     );
   }
 
+  function ItemDescription({item, display}:
+    {item: items.ItemClass, display: boolean}) {
+      if (!display) {
+        return <></>
+    }
+    return (
+      <div>Description</div>
+    )
+  }
 export default ItemContainer;
